d066af38bfb613f5e9b49539cb7d889654fc36de
Patrick Singer <sinpat224@hotmail.com>
2019-04-27 11:47:41 +0200

    Add graph structure

---
+++ src/graph.rs
@@ -0,0 +1,29 @@
+struct Node {
+    id: usize,
+    lat: f64,
+    long: f64,
+    height: f64,
+    ch_level: usize
+}
+
+struct HalfEdge {
+    target_id: usize,
+    edge_costs: [f64; 3]
+}
+
+struct Edge {
+    source_id: usize,
+    target_id: usize,
+    edge_costs: [f64; 3],
+    repl_edge_1: usize,
+    repl_edge_2: usize
+}
+
+struct Graph {
+    nodes: Vec<Node>,
+    edges: Vec<Edge>,
+    half_edges_in: Vec<HalfEdge>,
+    half_edges_out: Vec<HalfEdge>,
+    offsets_in: Vec<usize>,
+    offsets_out: Vec<usize>
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
+mod graph;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
 }
