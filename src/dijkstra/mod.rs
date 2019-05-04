5731226a8b64b7516c2ae31609c31608ab41d104
Patrick Singer <sinpat224@hotmail.com>
2019-05-04 18:39:07 +0200

    First attempt on bidirectional dijkstra

--- src/dijkstra/mod.rs
+++ src/dijkstra/mod.rs
@@ -1,66 +1,81 @@
 use crate::graph::Graph;
 use std::collections::binary_heap::BinaryHeap;
-use std::cmp::Ordering;
 
-use ordered_float::{OrderedFloat};
+use ordered_float::{ OrderedFloat };
 
-#[derive(PartialEq)]
-struct State {
-    node_id: usize,
-    cost: OrderedFloat<f64>
-}
+mod state;
 
-impl std::cmp::Eq for State {}
-
-impl std::cmp::Ord for State {
-    // switch comparison, because we want a min-heap
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.cost.cmp(&self.cost)
-    }
-}
-
-impl std::cmp::PartialOrd for State {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
+use state::State;
 
 // find shortest path by doing a dijkstra search
 pub fn find_shortest_path(graph: &Graph, source: usize, target: usize) -> Option<(Vec<usize>, OrderedFloat<f64>)> {
     println!("Running Dijkstra search...");
-    let mut dist = vec![(OrderedFloat(std::f64::NAN), None); graph.get_nodes().len()];
-    let mut heap = BinaryHeap::new();
-    dist[source] = (OrderedFloat(0.0), None);
-    heap.push(State {
-        node_id: source,
-        cost: OrderedFloat(0.0)
-    });
-    while let Some(State { node_id, cost }) = heap.pop() {
-        if node_id == target {
-            // unfold path recursively
-            let mut path = Vec::new();
-            let mut current_dist = dist[target];
-            path.push(target);
-            while let Some(prev) = current_dist.1 {
-                path.push(prev);
-                current_dist = dist[prev];
+    let mut dist_forward = vec![(OrderedFloat(std::f64::NAN), None); graph.get_nodes().len()];
+    let mut dist_backward = vec![(OrderedFloat(std::f64::NAN), None); graph.get_nodes().len()];
+    let mut candidates = BinaryHeap::new();
+    let mut last_node = (None, None);
+    dist_forward[source] = (OrderedFloat(0.0), None);
+    dist_backward[target] = (OrderedFloat(0.0), None);
+    candidates.push(State { node_id: source, cost: OrderedFloat(0.0), forward: true });
+    candidates.push(State { node_id: target, cost: OrderedFloat(0.0), forward: false });
+    while let Some(State { node_id, cost, forward }) = candidates.pop() {
+        if forward {
+            if cost > dist_forward[node_id].0 {
+                continue;
             }
-            path.reverse();
-            return Some((path, cost));
-        }
-        if cost > dist[node_id].0 {
-            continue
-        }
-        for edge in graph.get_edges_out(node_id) {
-            let next = State {
-                node_id: edge.get_target_id(),
-                cost: OrderedFloat(cost.0 + edge.calc_costs().0)
-            };
-            if next.cost < dist[next.node_id].0 {
-                dist[next.node_id] = (next.cost, Some(node_id));
-                heap.push(next);
+            last_node.0 = Some(node_id);
+            for half_edge in graph.get_edges_out(node_id) {
+                let next = State {
+                    node_id: half_edge.get_target_id(),
+                    cost: OrderedFloat(cost.0 + half_edge.calc_costs().0),
+                    forward
+                };
+                if next.cost < dist_forward[next.node_id].0 {
+                    dist_forward[next.node_id] = (next.cost, Some(node_id));
+                    candidates.push(next);
+                }
             }
+        }
+        if !forward {
+            if cost > dist_backward[node_id].0 {
+                continue;
+            }
+            last_node.1 = Some(node_id);
+            for half_edge in graph.get_edges_in(node_id) {
+                let next = State {
+                    node_id: half_edge.get_target_id(),
+                    cost: OrderedFloat(cost.0 + half_edge.calc_costs().0),
+                    forward
+                };
+                if next.cost < dist_backward[next.node_id].0 {
+                    dist_backward[next.node_id] = (next.cost, Some(node_id));
+                    candidates.push(next);
+                }
+            }
+        }
+        if last_node.0 == last_node.1 {
+            let path = construct_path(&dist_forward, &dist_backward, last_node.0.unwrap());
+            let dist_up = dist_forward[last_node.0.unwrap()].0;
+            let dist_down = dist_backward[last_node.0.unwrap()].0;
+            return Some((path, OrderedFloat(dist_up.0 + dist_down.0)));
         }
     }
     None
+}
+
+fn construct_path(dist_forward: &Vec<(OrderedFloat<f64>, Option<usize>)>, dist_backward: &Vec<(OrderedFloat<f64>, Option<usize>)>, node_id: usize) -> Vec<usize> {
+    let mut path = Vec::new();
+    let mut current_dist = dist_forward[node_id];
+    path.push(node_id);
+    while let Some(prev) = current_dist.1 {
+        path.push(prev);
+        current_dist = dist_forward[prev];
+    }
+    path.reverse();
+    current_dist = dist_backward[node_id];
+    while let Some(succ) = current_dist.1 {
+        path.push(succ);
+        current_dist = dist_backward[succ];
+    }
+    path
 }
---
+++ src/dijkstra/state.rs
@@ -0,0 +1,24 @@
+use std::cmp::Ordering;
+use ordered_float::{ OrderedFloat };
+
+#[derive(PartialEq)]
+pub struct State {
+    pub node_id: usize,
+    pub cost: OrderedFloat<f64>,
+    pub forward: bool
+}
+
+impl std::cmp::Eq for State {}
+
+impl std::cmp::Ord for State {
+    // switch comparison, because we want a min-heap
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
+}
+
+impl std::cmp::PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
