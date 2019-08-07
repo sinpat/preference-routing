e4f3f2f78977b07d25f406a2ffd9895a0090b7b3
Patrick Singer <st142778@stud.uni-stuttgart.de>
2019-08-07 10:40:19 +0200

    Use cmd param for graph file input

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
+use std::env;
+
 mod graph;
 mod helpers;
 mod lp;
 mod server;
 
-const GRAPH_FILE: &str = "/home/patrick/Uni/Bachelor/Bachelorarbeit/data/ba-wue/concGraph";
-
 const EDGE_COST_DIMENSION: usize = 3;
 const EDGE_COST_TAGS: [&str; EDGE_COST_DIMENSION] = ["Unsuit", "Distance", "Height"];
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Please provide exactly one parameter, which is the path to the graph file");
+    }
+    let graph_file = &args[1];
     let graph =
-        graph::parse_graph_file(GRAPH_FILE)
+        graph::parse_graph_file(graph_file)
             .unwrap();
     server::start_server(graph);
 }
