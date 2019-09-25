c31b875bf2204db2420b47bbf4385f7ec0741b11
Patrick Singer <st142778@stud.uni-stuttgart.de>
2019-09-25 17:48:19 +0200

    Merge routing and user scopes

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -13,8 +13,8 @@ use crate::user::UserState;
 // use actix_web::dev::{Service, ServiceResponse};
 // use futures::{Future, IntoFuture};
 
+mod auth;
 mod routing;
-mod user;
 
 pub struct AppState {
     graph: Graph,
@@ -22,101 +22,96 @@ pub struct AppState {
     users: Mutex<Vec<UserState>>,
 }
 
+impl AppState {
+    fn new(graph: Graph, database_path: String) -> Self {
+        println!("Reading user database...");
+        let users = match File::open(&database_path) {
+            Ok(mut file) => {
+                let mut content = String::new();
+                file.read_to_string(&mut content)
+                    .expect("Could not read file content");
+                let users: Vec<UserState> =
+                    serde_json::from_str(&content).expect("Could not deserialize file content");
+                users
+            }
+            Err(_) => {
+                println!("No database file existing");
+                vec![UserState::new(
+                    // test user
+                    String::from("test"),
+                    String::from("testtest"),
+                )]
+            }
+        };
+        AppState {
+            graph,
+            database_path,
+            users: Mutex::new(users),
+        }
+    }
+
+    fn write_to_file(&self) {
+        let mut file = File::create(&self.database_path).expect("Could not create file");
+        let buffer = serde_json::to_vec(&self.users).expect("Could not serialize state");
+        file.write_all(&buffer)
+            .expect("Could not write state to file");
+    }
+}
+
 pub fn start_server(graph: Graph, port: String, database_path: String) {
-    println!("Reading user database...");
-    let users = if let Some(state) = read_state_from_file(database_path.clone()) {
-        state
-    } else {
-        vec![UserState::new(
-            // test user
-            String::from("test"),
-            String::from("testtest"),
-        )]
-    };
-    let state = web::Data::new(AppState {
-        graph,
-        database_path,
-        users: Mutex::new(users),
-    });
+    let state = web::Data::new(AppState::new(graph, database_path));
     println!("Starting server");
     HttpServer::new(move || {
         App::new()
-            .wrap(Cors::new().allowed_origin("http://localhost:8080"))
             .register_data(state.clone())
-            .service(
-                web::scope("/routing")
-                    .wrap_fn(|req, srv| {
-                        srv.call(req).map(|res| {
-                            let req = res.request();
-                            let state: &AppState = req.app_data().unwrap();
-                            write_state_to_file(&state.database_path, &state.users.lock().unwrap());
-                            res
-                        })
-                    })
-                    /*
-                    .wrap_fn(|req, srv| {
-                        let unauth: Box<dyn IntoFuture<Item = ServiceResponse>> =
-                            Box::new(ServiceResponse::new(
-                                req.into_parts().0,
-                                HttpResponse::Unauthorized().finish(),
-                            ));
-                        let auth_header = req.headers().get("Authorization");
-                        match auth_header {
+            .wrap(Cors::new().allowed_origin("http://localhost:8080"))
+            .wrap_fn(|req, srv| {
+                srv.call(req).map(|res| {
+                    let req = res.request();
+                    let state: &AppState = req.app_data().unwrap();
+                    state.write_to_file();
+                    res
+                })
+            })
+            /*
+            .wrap_fn(|req, srv| {
+                let unauth: Box<dyn IntoFuture<Item = ServiceResponse>> =
+                    Box::new(ServiceResponse::new(
+                        req.into_parts().0,
+                        HttpResponse::Unauthorized().finish(),
+                    ));
+                let auth_header = req.headers().get("Authorization");
+                match auth_header {
+                    None => unauth,
+                    Some(value) => {
+                        let token = value.to_str().unwrap();
+                        let mut users = state.users.lock().unwrap();
+                        let user_state = users.iter_mut().find(|x| x.auth.token == token);
+                        match user_state {
                             None => unauth,
-                            Some(value) => {
-                                let token = value.to_str().unwrap();
-                                let mut users = state.users.lock().unwrap();
-                                let user_state = users.iter_mut().find(|x| x.auth.token == token);
-                                match user_state {
-                                    None => unauth,
-                                    Some(user) => Box::new(srv.call(req).map(|res| res)),
-                                }
-                            }
+                            Some(user) => Box::new(srv.call(req).map(|res| res)),
                         }
-                    })
-                    */
-                    .route("/closest", web::get().to(routing::find_closest))
-                    .route("/fsp", web::post().to(routing::fsp))
-                    .route("/preference", web::get().to(routing::get_preference))
-                    .route("/preference", web::post().to(routing::set_preference))
-                    .route("/find_preference", web::post().to(routing::find_preference))
-                    .route("/routes", web::get().to(routing::get_routes))
-                    .route("/reset", web::post().to(routing::reset_data)),
-            )
-            .service(
-                web::scope("/user")
-                    .route("/login", web::post().to(user::login))
-                    .route("/register", web::post().to(user::register)),
-            )
+                    }
+                }
+            })
+            */
+            // routing stuff
+            .route("/closest", web::get().to(routing::find_closest))
+            .route("/fsp", web::post().to(routing::fsp))
+            .route("/preference", web::get().to(routing::get_preference))
+            .route("/preference", web::post().to(routing::set_preference))
+            .route("/find_preference", web::post().to(routing::find_preference))
+            .route("/routes", web::get().to(routing::get_routes))
+            .route("/reset", web::post().to(routing::reset_data))
+
+            // auth stuff
+            .route("/login", web::post().to(auth::login))
+            .route("/register", web::post().to(auth::register))
     })
     .bind(format!("0.0.0.0:{}", port))
     .expect("Can not bind to port 8000")
     .run()
     .expect("Could not start sever");
-}
-
-fn read_state_from_file(file_path: String) -> Option<Vec<UserState>> {
-    match File::open(file_path) {
-        Ok(mut file) => {
-            let mut content = String::new();
-            file.read_to_string(&mut content)
-                .expect("Could not read file content");
-            let users: Vec<UserState> =
-                serde_json::from_str(&content).expect("Could not deserialize file content");
-            Some(users)
-        }
-        Err(_) => {
-            println!("No database file existing");
-            None
-        }
-    }
-}
-
-fn write_state_to_file(file_path: &str, state: &[UserState]) {
-    let mut file = File::create(file_path).expect("Could not create file");
-    let buffer = serde_json::to_vec(state).expect("Could not serialize state");
-    file.write_all(&buffer)
-        .expect("Could not write state to file");
 }
 
 #[cfg(test)]
